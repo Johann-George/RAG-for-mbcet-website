04c77c6b440ea2292b0bd8c1d825c04d9b5f930b
Johann-George <johanngeorge2003@gmail.com>
2024-08-11 20:48:15 +0530

    Rag implementation

---
+++ ingestion.py
@@ -0,0 +1,27 @@
+import os
+from dotenv import load_dotenv
+from langchain_community.document_loaders import TextLoader
+from langchain_text_splitters import CharacterTextSplitter
+from langchain_pinecone import PineconeVectorStore
+from langchain_ollama import OllamaEmbeddings
+
+load_dotenv()
+
+if __name__ == "__main__":
+    print("Ingestion...")
+    loader = TextLoader("/mbcet_website_data.txt")
+    document = loader.load()
+
+    print("splitting...")
+
+    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+    texts = text_splitter.split_documents(document)
+    print(f"created {len(texts)} chunks")
+
+    embeddings = OllamaEmbeddings(model="llama3")
+
+    print("ingesting..")
+
+    PineconeVectorStore.from_documents(texts, embeddings, index_name=os.environ['INDEX_NAME'])
+
+    print("finish")
---
+++ main.py
@@ -0,0 +1,65 @@
+import os
+
+from dotenv import load_dotenv
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import PromptTemplate
+from langchain_core.runnables import RunnablePassthrough
+from langchain_ollama import OllamaEmbeddings, ChatOllama
+from langchain_pinecone import PineconeVectorStore
+
+from langchain import hub
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain.chains.retrieval import create_retrieval_chain
+
+load_dotenv()
+
+
+def format_docs(docs):
+    return "\n\n".join(doc.page_content for doc in docs)
+
+
+if __name__ == "__main__":
+    print("Retrieving")
+
+    embeddings = OllamaEmbeddings(model='llama3')
+    llm = ChatOllama(model='llama3')
+
+    query = "Who is E-yanthra team?"
+    # chain = PromptTemplate.from_template(template=query) | llm
+    # result = chain.invoke(input={})
+    # print(result.content)
+
+    vectorStore = PineconeVectorStore(
+        index_name=os.environ["INDEX_NAME"], embedding=embeddings
+    )
+
+    # retrieval_qa_chat_prompt = hub.pull("langchain-ai/retrieval-qa-chat")
+    # combine_docs_chain = create_stuff_documents_chain(llm, retrieval_qa_chat_prompt)
+    # retrieval_chain = create_retrieval_chain(
+    #     retriever=vectorStore.as_retriever(), combine_docs_chain=combine_docs_chain
+    # )
+    #
+    # result = retrieval_chain.invoke(input={"input": query})
+    # print(result)
+
+    template = """
+    Act as a receptionist for Mar Baselios College of engineering and technology and use the following pieces of context
+    to answer the questions at the end. If you don't know the answer just say that you don't know,
+    don't try to make up an answer. Always say "thanks for asking!" at the end of the answer.
+    
+    {context}
+    
+    Question: {question}
+    
+    Helpful answer:"""
+
+    custom_rag_propmt = PromptTemplate.from_template(template)
+
+    rag_chain = (
+            {"context": vectorStore.as_retriever() | format_docs, "question": RunnablePassthrough()}
+            | custom_rag_propmt
+            | llm
+    )
+
+    res = rag_chain.invoke(query)
+    print(res.content)
